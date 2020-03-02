43a04619dfc39621d1165e5b0a106494997bc625
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2020-03-02 15:04:01 -0800

    Hack to add symbol interpolation

--- ast_tools/transformers/symbol_replacer.py
+++ ast_tools/transformers/symbol_replacer.py
@@ -1,12 +1,31 @@
 import ast
-from .node_replacer import NodeReplacer
+from copy import deepcopy
 
-class SymbolReplacer(NodeReplacer):
-    def _get_key(self, node):
-        if isinstance(node, ast.Name):
-            return node.id
-        else:
-            return None
+class SymbolReplacer(ast.NodeTransformer):
+    def __init__(self, symbol_table):
+        self.symbol_table = symbol_table
+
+    def visit_Name(self, node):
+        if node.id in self.symbol_table:
+            return deepcopy(self.symbol_table[node.id])
+        return node
+
+
+class SymbolReplacer_2(ast.NodeTransformer):
+    def __init__(self, symbol_table):
+        self.symbol_table = symbol_table
+
+    def visit_Attribute(self, node):
+        if 'symbol_interpolate' in self.symbol_table:
+            node.attr = node.attr.replace('symbol_interpolate', str(self.symbol_table['symbol_interpolate'].n))
+
+            if hasattr(node.value, "attr"):
+                node.value.attr = node.value.attr.replace('symbol_interpolate', str(self.symbol_table['symbol_interpolate'].n))
+
+        return node
+
 
 def replace_symbols(tree, symbol_table):
-    return SymbolReplacer(symbol_table).visit(tree)
+    temp = SymbolReplacer(symbol_table).visit(tree)
+    return SymbolReplacer_2(symbol_table).visit(temp)
+
