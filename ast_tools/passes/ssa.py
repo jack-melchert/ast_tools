933ef358b04381c53d495471340a796c929e55fe
jack-melchert <56329001+jack-melchert@users.noreply.github.com>
2020-10-16 11:39:54 -0500

    Update ssa.py

--- ast_tools/passes/ssa.py
+++ ast_tools/passes/ssa.py
@@ -329,12 +329,12 @@ def _prove_names_defined(
     if isinstance(node, ast.Name):
         if isinstance(node.ctx, ast.Store):
             names.add(node.id)
-        elif node.id not in names and node.id not in env and \
-                node.id not in env["__builtins__"].__dict__:
-            if hasattr(node, 'lineno'):
-                raise SyntaxError(f'Cannot prove name, {node.id}, is defined at line {node.lineno}')
-            else:
-                raise SyntaxError(f'Cannot prove name, {node.id}, is defined')
+        elif node.id not in names and node.id not in env:
+            if node.id not in env["__builtins__"].__dict__:
+                if hasattr(node, 'lineno'):
+                    raise SyntaxError(f'Cannot prove name, {node.id}, is defined at line {node.lineno}')
+                else:
+                    raise SyntaxError(f'Cannot prove name, {node.id}, is defined')
 
     elif isinstance(node, ast.If):
         t_returns = _always_returns(node.body)
